61451514bb55d9fa7278b0c6849248a0c0675f1d
“Zicheng <2915413y@student.gla.ac.uk>
2025-02-14 12:57:00 +0000

    c9.9f

--- rango/views.py
+++ rango/views.py
@@ -4,12 +4,21 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+# Use the login_required() decorator to ensure only those logged in can
+# access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
 
 def index(request):
 # Query the database for a list of ALL categories currently stored.
