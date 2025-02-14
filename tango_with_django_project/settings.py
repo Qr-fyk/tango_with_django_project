00f96d51e20789108ee51301b02dbeea9d3b7d23
“Zicheng <2915413y@student.gla.ac.uk>
2025-02-14 12:04:32 +0000

    c9.5f

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -76,6 +76,13 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
